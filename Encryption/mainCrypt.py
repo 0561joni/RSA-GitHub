cd0295157cd8de93680da7cb73c6417b78e3a762
0561joni <31111927+0561joni@users.noreply.github.com>
2017-08-20 15:04:10 +0200

    added commentaries

--- Encryption/mainCrypt.py
+++ Encryption/mainCrypt.py
@@ -1,6 +1,7 @@
 from Encryption import encryptor as encryptor
 from Encryption import decryptor as decryptor
 
-encryptor.encrypt("Ql(!O'6P@ILL)")
+encryptor.encrypt("Ql(!O'6P@ILL)23ddhqw4k_f-sg5")
 decryptor.decrypt()
 
+# decrypt, encrypt takes about 1s for primes up to 1 billion
--- KeyGeneration/mainKeyGen.py
+++ KeyGeneration/mainKeyGen.py
@@ -3,5 +3,7 @@
 from KeyGeneration import keyGen as keyGen
 from KeyGeneration import calcPrimes as calcPrimes
 
-print(keyGen.generateKeys())
-#calcPrimes.calculatePrimes(10000000)
+
+#calcPrimes.calculatePrimes(1000000000) # takes 24s for 1 billion
+print(keyGen.generateKeys()) # about 3s for primes up to 1 billion
+
